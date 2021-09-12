f47fcc12a429a94db0efb999c24ff056a68d0b29
robinsonvs <robinsonvs@gmail.com>
2021-09-12 02:10:57 -0300

    add second command to search servers names on internet

--- app/app.go
+++ app/app.go
@@ -13,17 +13,25 @@ func Generate() *cli.App {
 	app.Name = "Command Line Application"
 	app.Usage = "Search IPs and Names on Server"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search IPs",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search IPs",
+			Flags:  flags,
 			Action: searchIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Search Servers Names",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 
@@ -40,5 +48,18 @@ func searchIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
